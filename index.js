f827c524bf4bb2ea224ad793edbd2861f93e17f5
Valencia White <valenciawhite@rocketmail.com>
2022-03-10 19:19:18 -0500

    css/html updates

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function renderArtData(artData) {
     for (let i=0; i<12; i++) {
         {
             if ((artData.data[i].image_id) !== null) {
-                console.log(artData.data[i])
+                // console.log(artData.data[i])
                 // console.log(artData.data[i].artist_title)
                 const artIdElement = document.createElement("img")
-                console.log(artIdElement)
+                // console.log(artIdElement)
                 thumbnailElement.appendChild(artIdElement)
                 artIdElement.id = artData.data[i].id
                 artIdElement.src = `https://www.artic.edu/iiif/2/${artData.data[i].image_id}/full/843,/0/default.jpg`
@@ -23,6 +23,9 @@ function renderArtData(artData) {
 
 }
 
-function bigArt() {
-    console.log("i got clicked!")
+
+function bigArt(event) {
+ console.log("hello");
 }
+
+
